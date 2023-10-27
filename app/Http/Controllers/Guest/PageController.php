6444785011d14f1aae168ce0bfe75dd5b96b9d01
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-27 21:47:17 +0200

    add common controller and model

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Models\Movie;
 use Illuminate\Http\Request;
 
+/* I controller sono responsabili di gestire le richieste e preparare la risposta. */
 class PageController extends Controller
 {
+    /* Questo metodo viene chiamato quando una richiesta HTTP GET è indirizzata alla Route  */
     public function index()
     {
         return view('index', ['movies' => Movie::all()]);
+        /* Il modello Movie rappresenta i dati relativi ai film nel database e Movie::all() recupera tutti i film dal database. */
     }
 }
