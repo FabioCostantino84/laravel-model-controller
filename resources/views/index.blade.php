0ce9337c5d2e05300b4d07e9c878334de0279de5
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-27 21:36:09 +0200

    add common @extends @section @include

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,11 @@
-{{-- NON MI FUNZIONANO --}}
 
+{{-- Questa istruzione @extends il layout che si trova dentro "layouts/app.blade.php". --}}
 @extends('layouts.app')
 
+{{-- Le @section vengono utilizzate per inserire contenuti dinamici. --}}
 @section('title', 'Movies')
 
+{{-- Questo @section mi fa inserire il contenuto  di questa pagina all'interno del layout. --}}
 @section('content')
 
     <main>
@@ -11,6 +13,7 @@
             <h1 class="mt-3">MOVIES</h1>
             <div class="row row-cols-1 row-cols-md-2 row-cols-lg-5 g-4 my-3">
 
+                {{-- @include permette di inserire il contenuto di un file all'interno di questa pagina. --}}
                 @include('partials.cards')
 
             </div>
