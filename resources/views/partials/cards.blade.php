52d7b85176a0fd7421ceac12c80beba708528451
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-27 21:20:24 +0200

    add common code

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,10 +1,10 @@
-
-
+{{-- Questa è una direttiva Blade di Laravel che avvia un loop foreach sui dati contenuti nell'array $movies, assegnando ogni elemento a una variabile $movie. --}}
 @forelse ($movies as $movie)
     <div class="col">
         <div class="card shadow h-100">
             <div class="card-body">
 
+                {{-- L'URL dell'immagine è generato dinamicamente con un parametro random basato sull'ID del film. --}}
                 <img src="https://picsum.photos/400/200?random={{ $movie->id }}" class="card-img-top rounded-2 mb-3"
                     alt="{{ $movie->original_title }}">
 
@@ -16,7 +16,7 @@
             </div>
         </div>
     </div>
-
+    {{-- Questo è un'istruzione Blade che verifica se l'array $movies è vuoto. Se lo è, verrà eseguito il blocco successivo. --}}
 @empty
 
     <div class="col">
